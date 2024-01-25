7639abda46ac6f1d465f5abfea152cafc114838f
unknown <bry_of_the_sky@yahoo.com>
2024-01-25 22:54:19 +0800

    Fixed bug where control pool count does not reset on screen transition and eventually crashes the game or randomly changes the control

--- src/MapCommon.thumb.c
+++ src/MapCommon.thumb.c
@@ -49,6 +49,7 @@ void mapCommon_transferToMap(ScreenAttr *screenAttribute, CharacterCollection *c
 	sprite_palette_init();
 	mchar_reinit(characterCollection, &character);
 	
+	controlPool->currentCount = 0;
 	for (i = 0; i < characterCollection->countCharacterTransfer; ++i) {
 		characterCollection->characterTransfer[i](character, controlPool);
 	}
